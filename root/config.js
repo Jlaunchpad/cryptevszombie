5fc53a9ad1a9ddc2fa5000d3bda9b1b9d6dfdfcf
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-09-09 22:45:25 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,6 +1,6 @@
 {
   "start":             "Sat Sep 09 2021 22:00:00 GMT+0700",
   "end_round_1":       "Sat Sep 09 2021 22:20:00 GMT+0700",
-  "end_round_2":       "Sat Sep 09 2021 22:40:00 GMT+0700",
-  "end_waiting_claim": "Sat Sep 09 2021 23:00:00 GMT+0700"
+  "end_round_2":       "Sat Sep 09 2021 23:00:00 GMT+0700",
+  "end_waiting_claim": "Sat Sep 09 2021 23:05:00 GMT+0700"
 }
