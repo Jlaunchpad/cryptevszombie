c427951091c4fc0df3f18eb1f72664033354be7b
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-11-19 13:36:16 +0700

    Delete config-note.js

--- root/config-note.js
+++
@@ -1,6 +0,0 @@
-{
-  "start":             "Sat Sep 10 2021 11:00:00 GMT+0700",
-  "end_round_1":       "Sat Sep 11 2021 13:00:00 GMT+0700",
-  "end_round_2":       "Sat Sep 11 2021 22:00:00 GMT+0700",
-  "end_waiting_claim": "Sat Sep 11 2021 23:00:00 GMT+0700"
-}
