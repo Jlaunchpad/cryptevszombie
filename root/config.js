f691b89b20ddd94e87dda3e3ebb9204110170d81
Hao2505 <73099324+Hao2505@users.noreply.github.com>
2021-12-09 16:40:13 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,6 +1,6 @@
 {
-  "start":             "Dec 08 2021 21:00:00 GMT+0700",
-  "end_round_1":       "Dec 08 2021 21:30:00 GMT+0700",
-  "end_round_2":       "Dec 07 2020 15:44:01 GMT+0700",
-  "end_waiting_claim": "Dec 08 2021 21:30:00 GMT+0700"
+  "start":             "Thu Dec 10 2021 02:00:00 GMT",
+  "end_round_1":       "Thu Dec 10 2021 12:00:00 GMT",
+  "end_round_2":       "Dec 10 2020 15:44:01",
+  "end_waiting_claim": "Thu Dec 10 2021 14:00:00 GMT"
 }
